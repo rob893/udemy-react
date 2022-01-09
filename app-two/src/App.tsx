795fe373e8a515c07f9db766861f8f9c4cb9584c
Robert Herber <rwherber@gmail.com>
2022-01-08 20:25:37 -0800

    chore: more class

---
+++ app-two/src/App.tsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
+
+function App() {
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/" element={<AllMeetupsPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
